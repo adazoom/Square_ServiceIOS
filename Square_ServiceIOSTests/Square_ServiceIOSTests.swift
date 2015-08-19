bf557ed0a8631570f680204ad9feafd2117b266d
aidazhuma <zaida09@mail.ru>
2015-08-18 23:05:28 -0700

    Storyboard setup

---
+++ Square_ServiceIOSTests/Square_ServiceIOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Square_ServiceIOSTests.swift
+//  Square_ServiceIOSTests
+//
+//  Created by Aida Zhumabekova on 8/18/15.
+//  Copyright (c) 2015 Aida Zhumabekova. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Square_ServiceIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
