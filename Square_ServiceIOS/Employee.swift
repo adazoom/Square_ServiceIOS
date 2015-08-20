7b15f330caca0c717ba786ccd64008922a841f04
aidazhuma <zaida09@mail.ru>
2015-08-19 17:49:26 -0700

    First

---
+++ Square_ServiceIOS/Employee.swift
@@ -0,0 +1,28 @@
+//
+//  Employee.swift
+//  Square_ServiceIOS
+//
+//  Created by Aida Zhumabekova on 8/19/15.
+//  Copyright (c) 2015 Aida Zhumabekova. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+import MapKit
+
+class Employee: NSObject {
+    
+    var name: String
+    var activity: String
+    var numHours: Int
+    var image: UIImage
+    
+    init(employeeDictionary: NSDictionary) {
+        name = employeeDictionary["name"] as String
+        activity = employeeDictionary["activity"] as String
+        numHours = employeeDictionary["numberOfHours"] as Int
+        image = employeeDictionary["image"] as UIImage
+    }
+
+}
