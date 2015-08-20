fb2db531d05a907655c4b93f055b6b8bd9a7cf15
aidazhuma <zaida09@mail.ru>
2015-08-19 20:21:26 -0700

    error fixed

--- Square_ServiceIOS/DetailViewController.swift
+++ Square_ServiceIOS/DetailViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
@@ -23,9 +20,6 @@ class DetailViewController: UIViewController {
     func configureView() {
         // Update the user interface for the detail item.
         if let detail: AnyObject = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
         }
     }
 
