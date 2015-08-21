667b0da29513eec976bddaa852d8cdd8691af8c5
Shivani Mall <shivanimall1494@gmail.com>
2015-08-21 01:11:10 -0700

    table employee

--- Square_ServiceIOS/MasterViewController.swift
+++ Square_ServiceIOS/MasterViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var detailViewController: DetailViewController? = nil
-    
+    var urlSession: NSURLSession!
     var objects = NSMutableArray()
 
 
@@ -47,22 +47,33 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
     // MARK: - Table View
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 6
     }
     
+    var employees: Array<Dictionary<String, NSObject>> = [
+        ["hours" : "12", "activity" : "Code Camp", "name" : "Jack" ],
+        ["hours" : "14", "activity" : "Child Care", "name" : "Sarah" ],
+        ["hours" : "16", "activity" : "Hackathon", "name" : "Alyssa" ],
+        ["hours" : "18", "activity" : "Baby Sit", "name" : "Vanessa" ],
+        ["hours" : "20", "activity" : "Education", "name" : "Gloria" ],
+        ["hours" : "12", "activity" : "Tech Tour", "name" : "Mia" ]
+    ]
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-       /* var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("EmployeeCell") as! UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("EmployeeCell", forIndexPath: indexPath) as! EmployeeCell
+        var employee = employees[indexPath.row]
+        cell.employeeLabel.text = employee["name"] as? String
+        cell.employeeHours.text = employee["hours"] as? String
+        cell.employeeOpportunity.text = employee["activity"] as? String
         
-        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
         
-       return cell*/
-        return UITableViewCell()
+
+//        return UITableViewCell()
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
     }
-
-
 }
 
